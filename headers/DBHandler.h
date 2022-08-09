722f9654e414f68b24b64ddb4b6057ddb54ca35d
jakub_gorsky <kuba.gorsky@gmail.com>
2022-08-09 12:18:29 +0200

    Can update based on ID or name.

--- headers/DBHandler.h
+++ headers/DBHandler.h
@@ -7,6 +7,7 @@
 
 #include <string>
 #include <sstream>
+#include <iostream>
 
 #include "sqlite3/sqlite3.h"
 
@@ -54,14 +55,10 @@ public:
         std::string statement = "SELECT * FROM Products;";
         return ExecQuery(statement);
     }
-//    std::stringstream Update(){
-//        std::stringstream log;
-//        if (!open){
-//            log << "Unable to open db.\n";
-//            return log;
-//        }
-//        return log;
-//    }
+
+    std::stringstream Update(){
+        return ExecQuery(UpdateInterface());
+    }
     ~DBHandler() = default;
 private:
     sqlite3 *db;
@@ -69,11 +66,90 @@ private:
     const char* filename;
     bool open{};
 
+    struct Product {
+        int ID{};
+        std::string Name;
+        double Price{};
+        long Qty{};
+        ~Product() = default;
+    };
+
+    std::string UpdateInterface(){
+        std::string input{}, statement{};
+        Product temp;
+        std::cout << "\n\tDo you want to search for the items?\n"
+                     "\t\t(Y) Yes\n"
+                     "\t\t(N) No\n";
+        std::cin >> input;
+        if (input == "Y" || input == "y"){
+            std::cout << "\n\tDo you want to search by ID or the name?\n"
+                         "\t\t(1) ID\n"
+                         "\t\t(2) Name\n";
+            std::cin >> input;
+            if (input == "1"){
+                std::cout << "\n\tProvide the ID of the product you want to search for:\n\t\t";
+                std::cin >> input;
+                std::cout << Select(std::stoi(input)).rdbuf();
+            }
+            else if (input == "2"){
+                std::cout << "\n\tProvide the Name of the product you want to search for:\n\t\t";
+                std::cin >> input;
+                std::cout << Select(input).rdbuf();
+            }
+            goto updating;
+        }
+        else if (input == "N" || input == "n"){
+            updating:
+            std::cout << "\n\tPlease, provide the ID of the product you wish to update:\n\t";
+            std::cin >> temp.ID;
+            std::cout << "\n\tIf you do not wish to update a field put a '-' in."
+                         "\n\n\tProvide new name: \n";
+            std::cin >> temp.Name;
+            std::cout << "\n\tProvide new price: \n";
+            std::cin >> temp.Price;
+            std::cout << "\n\tProvide new quantity in stock: \n";
+            std::cin >> temp.Qty;
+            std::cout << "\n\tPlease, review the changes: \n"
+                         "\t\tName: " + temp.Name + "\n" +
+                         "\t\tPrice: " + std::to_string(temp.Price) + "\n" +
+                         "\t\tQuantity: " + std::to_string(temp.Qty) + "\n" +
+                         "\n\tDo you want to commit the changes? (Y/N):";
+            std::cin >> input;
+            if (input == "N" || input == "n")
+                return "-";
+            else if (input == "Y" || input == "y"){
+                if (temp.Name == "-" && std::to_string(temp.Price) == "-" && std::to_string(temp.Qty) == "-")
+                    return "-";
+                if (temp.Name != "-" && std::to_string(temp.Price) == "-" && std::to_string(temp.Qty) == "-")
+                    statement = "UPDATE Products SET Name = " + sqlquote(temp.Name) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if(temp.Name != "-" && std::to_string(temp.Price) != "-" && std::to_string(temp.Qty) == "-")
+                    statement = "UPDATE Products SET Name = " + sqlquote(temp.Name) + ", Price = " + std::to_string(temp.Price) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if (temp.Name != "-" && std::to_string(temp.Price) != "-" && std::to_string(temp.Qty) != "-")
+                    statement = "UPDATE Products SET Name = " + sqlquote(temp.Name) + ", Price = " + std::to_string(temp.Price) + ", Qty = " + std::to_string(temp.Qty) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if (temp.Name != "-" && std::to_string(temp.Price) == "-" && std::to_string(temp.Qty) != "-")
+                    statement = "UPDATE Products SET Name = " + sqlquote(temp.Name) + ", Qty = " + std::to_string(temp.Qty) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if (temp.Name == "-" && std::to_string(temp.Price) != "-" && std::to_string(temp.Qty) != "-")
+                    statement = "UPDATE Products SET Price = " + std::to_string(temp.Price) + ", Qty = " + std::to_string(temp.Qty) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if (temp.Name == "-" && std::to_string(temp.Price) != "-" && std::to_string(temp.Qty) == "-")
+                    statement = "UPDATE Products SET Price = " + std::to_string(temp.Price) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else if (temp.Name == "-" && std::to_string(temp.Price) == "-" && std::to_string(temp.Qty) != "-")
+                    statement = "UPDATE Products SET Qty = " + std::to_string(temp.Qty) + " WHERE ID = " + std::to_string(temp.ID) + ";";
+                else
+                    return "-";
+            }
+        }
+        return statement;
+    }
+
     std::stringstream ExecQuery(const std::string& statement){
         std::stringstream log;
         int err{};
         if (!open){
             log << "Unable to open db.\n";
+            return log;
+        }
+        if (statement == "-"){
+            log << "[DBHANDLER] Error processing statement.";
             return log;
         }
         if(sqlite3_open_v2(filename, &db, SQLITE_OPEN_READWRITE, nullptr) != SQLITE_OK){
@@ -99,6 +175,7 @@ private:
             return log;
         }
         sqlite3_close_v2(db);
+        log << "[SQLITE3 Query] Query performed successfully.\n";
         return log;
     }
     static std::string sqlquote(const std::string &value){
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@ int main() {
     std::string name, price, qty;
 
     DBHandler DB(db, "../DB/identifier.sqlite");
-    std::cout << "Provide data: \n";
-    std::cin >> name >> price >> qty;
-    std::cout << DB.Insert(name, std::stod(price), std::stoi(qty)).str();
-    std::cout << DB.SelectAll().rdbuf();
+//    std::cout << "Provide data: \n";
+//    std::cin >> name >> price >> qty;
+//    std::cout << DB.Insert(name, std::stod(price), std::stoi(qty)).str();
+//    std::cout << DB.SelectAll().rdbuf();
 //    std::cout << DB.Select("").rdbuf();
+//    DB.UpdateInterface();
+    std::cout << DB.Update().rdbuf();
     return 0;
 }
